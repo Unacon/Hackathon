d2b9a03611996d1e527deb58af2254c2ee1dd97a
Joao Vitor <joaovitor_892@hotmail.com>
2021-04-25 22:24:23 -0300

    arrumando

--- script/index.js
+++ script/index.js
@@ -4,7 +4,7 @@ console.log(slogan);
 let i = 1;
 
 setTimeout(() => {
-    url = "http://127.0.0.1:5500/page1.html";
+    url = "https://unacon.github.io/Hackathon/page1.html";
     window.location.assign(url)
 },2000);
 
